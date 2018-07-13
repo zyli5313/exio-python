abc52361195b2a82262ff32d5d9509e7165f79bd
Jake Li <jli@tolotechnologies.com>
2018-07-13 17:24:27 +0800

    restructured order book and error prone

--- exio/orderBook.py
+++ exio/orderBook.py
@@ -1,31 +1,49 @@
 #
 # Live order book updated from the exio Websocket Feed
 
-from sortedcontainers import SortedDict
-from decimal import Decimal
-from publicClient import PublicClient
 import json
+from sortedcontainers import SortedDict
+# from decimal import Decimal
+from publicClient import PublicClient
+from llist import dllist, dllistnode
 
+def numDigits(tickSize):
+  numDigit = 0
+  while tickSize < 1 - 1e-6:
+    tickSize *= 10
+    numDigit += 1
+
+  return numDigit
 
 class OrderBook(object):
 
-  def __init__(self, symbol='btc-usdt', log_to=None):
-    self._symbol = symbol
-    self._asks = SortedDict()
-    self._bids = SortedDict()
-    self._client = PublicClient()
-    self._sequence = -1
-    self._log_to = log_to
-    if self._log_to:
-      assert hasattr(self._log_to, 'write')
-    self._current_ticker = None
+  def __init__(self, symbol, tickSize):
+    self.symbol = symbol
+    self.tickSize = tickSize
+    self.asks = SortedDict()
+    self.bids = SortedDict()
+    self.client = PublicClient()
+    self.sequence = -1
 
     self.isReady = False
 
+    self.numDigit = numDigits(tickSize)
+    self.multiplier = 10 ** self.numDigit
+
+    # latest values of bid-ask spread
+    self.bidPxPre = 0.0
+    self.askPxPre = 0.0
+    self.bidSizePre = 0
+    self.askSizePre = 0
+
+    # <oid, node>
+    self.seqToOrderDict = {}
+    self.seqToPxDict = {} # internal px, not real px
+
   # def reset_book(self):
-  #     self._asks = SortedDict()
-  #     self._bids = SortedDict()
-  #     res = self._client.get_product_order_book(symbol=self.symbol, level=3)
+  #     self.asks = SortedDict()
+  #     self.bids = SortedDict()
+  #     res = self.client.get_product_order_book(symbol=self.symbol, level=3)
   #     for bid in res['bids']:
   #         self.add({
   #             'id': bid[2],
@@ -40,122 +58,155 @@ class OrderBook(object):
   #             'price': Decimal(ask[0]),
   #             'size': Decimal(ask[1])
   #         })
-  #     self._sequence = res['sequence']
+  #     self.sequence = res['sequence']
 
   def onUpdate(self, message):
-    if self._log_to:
-      pickle.dump(message, self._log_to)
 
-    print json.dumps(message, indent=2)
+    if message["type"] == "bookOrders":
+      for order in message["orders"]:
+        self.add(order)
 
-    if "sequence" not in message:
+      self.sequence = 0
+      print self.printBook()
       return
 
-    sequence = message['sequence']
-    # if self._sequence == -1:
+    sequence = int(message['sequence'])
+    # if self.sequence == -1:
     #     self.reset_book()
     #     return
-    if sequence <= self._sequence:
+    if sequence <= self.sequence:
       # ignore older messages (e.g. before order book initialization from
       # getProductOrderBook)
       return
-    elif sequence > self._sequence + 1:
-      self.onSequenceGap(self._sequence, sequence)
+    elif self.sequence != 0 and sequence > self.sequence + 1:
+      self.onSequenceGap(self.sequence, sequence)
       # return
 
-    msg_type = message['type']
-    if msg_type == 'add':
+    msgType = message['type']
+    if msgType == 'add':
       self.add(message)
-    elif msg_type == 'remove':
+    elif msgType == 'remove':
       self.remove(message)
-    elif msg_type == 'trade':
+    elif msgType == 'trade':
       self.trade(message)
-      self._current_ticker = message
-    # elif msg_type == 'change':
+    # elif msgType == 'change':
     #     self.change(message)
 
-    self._sequence = sequence
+    self.sequence = sequence
 
   def onSequenceGap(self, gap_start, gap_end):
     # self.reset_book()
     print('Error: messages missing ({} - {}). Re-initializing  book at sequence.'.format(
-        gap_start, gap_end, self._sequence))
+        gap_start, gap_end, self.sequence))
 
     exit(-1)
 
   def add(self, order):
     order = {
-        'oid': order['oid'],
-        'side': order['side'],
-        'price': Decimal(order['price']),
-        'size': Decimal(order.get('size') or order['remaining_size'])
+        'oid': str(order['oid']),
+        "sequence": int(order["sequence"]),
+        'side': str(order['side']),
+        # 'price': Decimal(order['price']),
+        # 'size': Decimal(order.get('size'))
+        "price": int(float(order["price"]) * self.multiplier),
+        "size": float(order["size"])
     }
-    if order['side'] == 'buy':
-      bids = self.get_bids(order['price'])
-      if bids is None:
-        bids = [order]
-      else:
-        bids.append(order)
-      self.set_bids(order['price'], bids)
-    else:
-      asks = self.get_asks(order['price'])
-      if asks is None:
-        asks = [order]
-      else:
-        asks.append(order)
-      self.set_asks(order['price'], asks)
 
-    if len(self._bids) > 0 and len(self._asks) > 0:
+    node = dllistnode(order)
+    self.seqToOrderDict[order["sequence"]] = node
+    self.seqToPxDict[order["sequence"]] = order["price"]
+
+    if order['side'] == 'buy':
+      bids = self.getBids(order['price'])
+      
+      if bids is None:
+        bids = dllist([node])
+      else:
+        bids.append(node)
+
+      self.setBids(order['price'], bids)
+    else:
+      asks = self.getAsks(order['price'])
+      
+      if asks is None:
+        asks = dllist([node])
+      else:
+        asks.append(node)
+      self.setAsks(order['price'], asks)
+
+    if len(self.bids) > 0 and len(self.asks) > 0:
       self.isReady = True
 
   def remove(self, order):
-    price = Decimal(order['price'])
-    if order['side'] == 'buy':
-      bids = self.get_bids(price)
-      if bids is not None:
-        bids = [o for o in bids if o['oid'] != order['oid']]
-        if len(bids) > 0:
-          self.set_bids(price, bids)
-        else:
-          self.remove_bids(price)
-    else:
-      asks = self.get_asks(price)
-      if asks is not None:
-        asks = [o for o in asks if o['oid'] != order['oid']]
-        if len(asks) > 0:
-          self.set_asks(price, asks)
-        else:
-          self.remove_asks(price)
+    sequence = int(order["sequence"])
 
-    if len(self._bids) == 0 or len(self._asks) == 0:
-      self.isReady = False
+    if sequence in self.seqToOrderDict:
+      node = self.seqToOrderDict[sequence]
+      price = self.seqToPxDict[sequence]
+
+      if order['side'] == 'buy':
+        bids = self.getBids(price)
+        if bids is not None:
+          bids.remove(node)
+
+          if len(bids) > 0:
+            self.setBids(price, bids)
+          else:
+            self.removeBids(price)
+      else:
+        asks = self.getAsks(price)
+        if asks is not None:
+          asks.remove(node)
+          if len(asks) > 0:
+            self.setAsks(price, asks)
+          else:
+            self.removeAsks(price)
+
+      if len(self.bids) == 0 or len(self.asks) == 0:
+        self.isReady = False
+    
+      del self.seqToOrderDict[sequence]
+      del self.seqToPxDict[sequence]
+    else:
+      raise Exception(order)
 
   def trade(self, order):
-    size = Decimal(order['size'])
-    price = Decimal(order['price'])
+    price = int(float(order["price"]) * self.multiplier)
+    size = float(order["size"])
+    sequence = int(order["sequence"])
+
+    node = self.seqToOrderDict[sequence]
 
     if order['side'] == 'buy':
-      bids = self.get_bids(price)
+      bids = self.getBids(price)
       if not bids:
         return
-      assert bids[0]['id'] == order['maker_order_id']
+      assert bids[0]['id'] == str(order['oid'])
+
       if bids[0]['size'] == size:
-        self.set_bids(price, bids[1:])
+        self.setBids(price, bids[1:])
+
+        del self.seqToOrderDict[sequence]
+        del self.seqToPxDict[sequence]
       else:
         bids[0]['size'] -= size
-        self.set_bids(price, bids)
+        self.setBids(price, bids)
     else:
-      asks = self.get_asks(price)
+      asks = self.getAsks(price)
       if not asks:
         return
-      assert asks[0]['id'] == order['maker_order_id']
+      assert asks[0]['id'] == str(order['oid'])
+
       if asks[0]['size'] == size:
-        self.set_asks(price, asks[1:])
+        self.setAsks(price, asks[1:])
+
+        del self.seqToOrderDict[sequence]
+        del self.seqToPxDict[sequence]
       else:
         asks[0]['size'] -= size
-        self.set_asks(price, asks)
+        self.setAsks(price, asks)
 
-    if len(self._bids) == 0 or len(self._asks) == 0:
+    if len(self.bids) == 0 or len(self.asks) == 0:
       self.isReady = False
 
   # def change(self, order):
@@ -170,21 +221,21 @@ class OrderBook(object):
   #         return
 
   #     if order['side'] == 'buy':
-  #         bids = self.get_bids(price)
+  #         bids = self.getBids(price)
   #         if bids is None or not any(o['id'] == order['order_id'] for o in bids):
   #             return
   #         index = [b['id'] for b in bids].index(order['order_id'])
   #         bids[index]['size'] = new_size
-  #         self.set_bids(price, bids)
+  #         self.setBids(price, bids)
   #     else:
-  #         asks = self.get_asks(price)
+  #         asks = self.getAsks(price)
   #         if asks is None or not any(o['id'] == order['order_id'] for o in asks):
   #             return
   #         index = [a['id'] for a in asks].index(order['order_id'])
   #         asks[index]['size'] = new_size
-  #         self.set_asks(price, asks)
+  #         self.setAsks(price, asks)
 
-  #     tree = self._asks if order['side'] == 'sell' else self._bids
+  #     tree = self.asks if order['side'] == 'sell' else self.bids
   #     node = tree.get(price)
 
   #     if node is None or not any(o['id'] == order['order_id'] for o in node):
@@ -193,42 +244,42 @@ class OrderBook(object):
   # def get_current_ticker(self):
   #     return self._current_ticker
 
-  def getCurrentBook(self):
-    result = {
-        'sequence': self._sequence,
-        'asks': [],
-        'bids': [],
-    }
-    for ask in self._asks:
-      try:
-        # There can be a race condition here, where a price point is removed
-        # between these two ops
-        this_ask = self._asks[ask]
-      except KeyError:
-        continue
-      for order in this_ask:
-        result['asks'].append([order['price'], order['size'], order['id']])
-    for bid in self._bids:
-      try:
-        # There can be a race condition here, where a price point is removed
-        # between these two ops
-        this_bid = self._bids[bid]
-      except KeyError:
-        continue
+  # def getCurrentBook(self):
+  #   result = {
+  #       'sequence': self.sequence,
+  #       'asks': [],
+  #       'bids': [],
+  #   }
+  #   for ask in self.asks:
+  #     try:
+  #       # There can be a race condition here, where a price point is removed
+  #       # between these two ops
+  #       this_ask = self.asks[ask]
+  #     except KeyError:
+  #       continue
+  #     for order in this_ask:
+  #       result['asks'].append([order['price'], order['size'], order['id']])
+  #   for bid in self.bids:
+  #     try:
+  #       # There can be a race condition here, where a price point is removed
+  #       # between these two ops
+  #       this_bid = self.bids[bid]
+  #     except KeyError:
+  #       continue
 
-      for order in this_bid:
-        result['bids'].append([order['price'], order['size'], order['id']])
-    return result
+  #     for order in this_bid:
+  #       result['bids'].append([order['price'], order['size'], order['id']])
+  #   return result
 
   def printBook(self, numLevels=5):
     strs = ""
-    for px, sizes in self._asks.nsmallest(numLevels):
-      size = float(sum([s['size'] for s in sizes]))
+    for px, sizes in self.asks.items()[:numLevels]:
+      size = sum([s['size'] for s in sizes])
       strs = "%.06f@%.2f\n" % (size, px) + strs
 
     strs += "---\n"
-    for px, sizes in self._bids.nlargest(numLevels):
-      size = float(sum([s['size'] for s in sizes]))
+    for px, sizes in self.bids.items()[::-1][:numLevels]:
+      size = sum([s['size'] for s in sizes])
       strs += "%.06f@%.2f\n" % (size, px)
 
     strs += "===\n"
@@ -236,25 +287,31 @@ class OrderBook(object):
     return strs
 
   def getAsk(self):
-    return self._asks.peekitem(0)[0]
+    return self.asks.peekitem(0)[0] * 1.0 / self.multiplier
 
-  def getAsks(self, price):
-    return self._asks.get(price)
+  def getAsks(self, priceIn):
+    price = int(float(priceIn) *  self.multiplier)
+    return self.asks.get(price)
 
-  def removeAsks(self, price):
-    del self._asks[price]
+  def removeAsks(self, priceIn):
+    price = int(float(priceIn) *  self.multiplier)
+    del self.asks[price]
 
-  def setAsks(self, price, asks):
-    self._asks[price] = asks
+  def setAsks(self, priceIn, asks):
+    price = int(float(priceIn) *  self.multiplier)
+    self.asks[price] = asks
 
   def getBid(self):
-    return self._bids.peekitem(-1)[0]
+    return self.bids.peekitem(-1)[0] * 1.0 / self.multiplier
 
-  def getBids(self, price):
-    return self._bids.get(price)
+  def getBids(self, priceIn):
+    price = int(float(priceIn) *  self.multiplier)
+    return self.bids.get(price)
 
-  def removeBids(self, price):
-    del self._bids[price]
+  def removeBids(self, priceIn):
+    price = int(float(priceIn) *  self.multiplier)
+    del self.bids[price]
 
-  def setBids(self, price, bids):
-    self._bids[price] = bids
+  def setBids(self, priceIn, bids):
+    price = int(float(priceIn) *  self.multiplier)
+    self.bids[price] = bids
--- exio/simpleClient.py
+++ exio/simpleClient.py
@@ -6,9 +6,15 @@ import logging
 import random
 import json
 from orderBook import OrderBook
+from publicClient import PublicClient
 from websocketClient import WebsocketClient
 from authClient import AuthenticatedClient
 from exioAuth import getAuthHeaders
+
+BOOK_MSG_TYPES = set(["bookOrders", "add", "remove", "trade"])
+ORDER_MSG_TYPES = set(["openOrders", "accepted", "rejected", "canceled", "executed"])
+MISC_MSG_TYPES = set(["heartbeat"])
+ALL_MSG_TYPES = BOOK_MSG_TYPES | ORDER_MSG_TYPES | MISC_MSG_TYPES
 
 def setupLogger(loggerName, logFile, level=logging.DEBUG):
   logger = logging.getLogger(loggerName)
@@ -26,36 +32,29 @@ def setupLogger(loggerName, logFile, level=logging.DEBUG):
 
   return logger
 
-def numDigits(tickSize):
-  numDigit = 0
-  while tickSize < 1 - 1e-6:
-    tickSize *= 10
-    numDigit += 1
-
-  return numDigit
 
 class SimpleClient(WebsocketClient):
   def __init__(self, key="qWivyDdti2mDkYQb0O/3+E8k6SyViHqzsfzIPsBKZXU=", 
           secret="RaBgGrKCkAk2NOeIC6lmZ3drz9W5b9B2MzD4QqAxHtBahB+v31SLmm/+q461EAcpQMb3nn8MbLcu5dDVHF7JdKVw2zI+BO93UpHSnrHUTjqY3afqynM+VQ==", 
-          symbol='eth-btc', 
+          symbol='btc-usdt', 
           logFile=None):
     super(SimpleClient, self).__init__(symbols=symbol, 
       auth=True,
       key=key,
       secret=secret,
       channels=[{"name": "books", "symbols": [symbol]}, {"name": "orders", "symbols": [symbol]}])
-    
+      # channels=[{"name": "books", "symbols": [symbol]}])
+
     self.symbol = symbol
     self.router = AuthenticatedClient(key, secret)
+    self.publicClient = PublicClient()
 
-    # print self.router.auth()
+    self.tickSize, self.size = self.publicClient.getTickSize(self.symbol)
 
     # self.orderBookDict = {}
     # <symbol index, OrderBook>
     # self.orderBookDict[0] = OrderBook(symbol=symbol)
-    self.orderBook = OrderBook(symbol=symbol)
-
-    self.tickSize, self.size = self.orderBook._client.getTickSize(self.symbol)
+    self.orderBook = OrderBook(symbol, self.tickSize)
 
     if logFile is None:
       self.logFile = "../log/bot_%s.log" % (dt.datetime.today().strftime("%Y%m%d"))
@@ -75,6 +74,15 @@ class SimpleClient(WebsocketClient):
 
     random.seed(100)
 
+  def randChoice(self, weights):
+    """weights must sum to 1"""
+    assert np.sum(weights) == 1, np.sum(weights)
+
+    cs = np.cumsum(weights)
+    idx = sum(cs < random.random())
+
+    return idx
+
   def onOpen(self):
     self._sequence = -1
     print("-- SimpleClient Started! --\n")
@@ -84,87 +92,101 @@ class SimpleClient(WebsocketClient):
 
 
   def onUpdate(self, message):
-    self.orderBook.onUpdate(message)
 
-    if not self.orderBook.isReady:
-      return
+    print json.dumps(message, indent=2)
 
-    # calc fair price to cross
-    bid = self.orderBook.getBid()
-    bids = self.orderBook.getBids(bid)
-    bidSize = float(sum([b['size'] for b in bids]))
-    ask = self.orderBook.getAsk()
-    asks = self.orderBook.getAsks(ask)
-    askSize = float(sum([a['size'] for a in asks]))
-    bid = float(bid)
-    ask = float(ask)
+    if message["type"] in BOOK_MSG_TYPES:
+      self.orderBook.onUpdate(message)
 
-    if np.isclose(self.bidPxPre, bid) and np.isclose(self.askPxPre, ask) and self.bidSizePre == bidSize and self.askSizePre == askSize:
-      return 
+      if not self.orderBook.isReady:
+        return
 
-    # get action idx
-    idx = self.randChoice(self.weights)
+      # calc fair price to cross
+      bid = self.orderBook.getBid()
+      bids = self.orderBook.getBids(bid)
+      bidSize = float(sum([b['size'] for b in bids]))
+      ask = self.orderBook.getAsk()
+      asks = self.orderBook.getAsks(ask)
+      askSize = float(sum([a['size'] for a in asks]))
+      bid = float(bid)
+      ask = float(ask)
 
-    if idx == 0:
+      if np.isclose(self.orderBook.bidPxPre, bid) and np.isclose(self.orderBook.askPxPre, ask) and self.orderBook.bidSizePre == bidSize and self.orderBook.askSizePre == askSize:
+        return 
+
+      # get action idx
+      idx = self.randChoice(self.weights)
+
+      if idx == 0:
+        pass
+      elif idx == 1:
+        self.logger.info(self.orderBook.printBook())
+
+        if bidSize > askSize:
+          side = "buy"
+          size = min(self.size, askSize)
+          px = ask
+
+          order = self.router.buyIOC(self.symbol, px, size)
+
+        else:
+          side = "sell"
+          size = min(self.size, bidSize)
+          px = bid
+
+          order = self.router.sellIOC(self.symbol, px, size)
+
+        self.logger.info("taker %s side=%s px=%s size=%s . order=%s" % (self.descriptions[idx], side, px, size, order))
+
+      elif idx == 2:
+        self.logger.info(self.orderBook.printBook())
+
+        ask2 = self.orderBook._asks.succ_key(self.getAsk())
+        bid2 = self.orderBook._bids.prev_key(self.getBid())
+        bids2 = self.getBids(bid2)
+        asks2 = self.getAsks(ask2)
+        bidSize2 = float(sum([b['size'] for b in bids2]))
+        askSize2 = float(sum([a['size'] for a in asks2]))
+        ask2 = float(ask2)
+        bid2 = float(bid2)
+
+        if bidSize > askSize:
+          side = "buy"
+          size = askSize + min(self.size, askSize2)
+          px = ask2
+
+          order = self.router.buyIOC(self.symbol, px, size)
+        else:
+          side = "sell"
+          size = bidSize + min(self.size, bidSize2)
+          px = bid2
+
+          order = self.router.sellIOC(self.symbol, px, size)
+
+        self.logger.info("taker %s side=%s px=%s size=%s . order=%s" % (self.descriptions[idx], side, px, size, order))
+
+
+      self.orderBook.bidPxPre = bid
+      self.orderBook.askPxPre = ask
+      self.orderBook.bidSizePre = bidSize
+      self.orderBook.askSizePre = askSize
+
+    elif message["type"] in ORDER_MSG_TYPES:
+      # print json.dumps(message, indent=2)
       pass
-    elif idx == 1:
-      self.logger.info(self.orderBook.printBook())
 
-      if bidSize > askSize:
-        side = "buy"
-        size = min(self.size, askSize)
-        px = ask
+    elif message["type"] in MISC_MSG_TYPES:
+      # print json.dumps(message, indent=2)
+      pass
 
-        order = self.router.buyIOC(self.symbol, px, size)
-
-      else:
-        side = "sell"
-        size = min(self.size, bidSize)
-        px = bid
-
-        order = self.router.sellIOC(self.symbol, px, size)
-
-      self.logger.info("taker %s side=%s px=%s size=%s . order=%s" % (self.descriptions[idx], side, px, size, order))
-
-    elif idx == 2:
-      self.logger.info(self.orderBook.printBook())
-
-      ask2 = self.orderBook._asks.succ_key(self.getAsk())
-      bid2 = self.orderBook._bids.prev_key(self.getBid())
-      bids2 = self.getBids(bid2)
-      asks2 = self.getAsks(ask2)
-      bidSize2 = float(sum([b['size'] for b in bids2]))
-      askSize2 = float(sum([a['size'] for a in asks2]))
-      ask2 = float(ask2)
-      bid2 = float(bid2)
-
-      if bidSize > askSize:
-        side = "buy"
-        size = askSize + min(self.size, askSize2)
-        px = ask2
-
-        order = self.router.buyIOC(self.symbol, px, size)
-      else:
-        side = "sell"
-        size = bidSize + min(self.size, bidSize2)
-        px = bid2
-
-        order = self.router.sellIOC(self.symbol, px, size)
-
-      self.logger.info("taker %s side=%s px=%s size=%s . order=%s" % (self.descriptions[idx], side, px, size, order))
-
-
-    self.bidPxPre = bid
-    self.askPxPre = ask
-    self.bidSizePre = bidSize
-    self.askSizePre = askSize
-
+    else:
+      raise Exception("Error! Unknown message type. %s" % (json.dumps(message, indent=2)))      
 
 
 
 if __name__ == '__main__':
     
-  client = SimpleClient(symbol="eth-btc")
+  client = SimpleClient(symbol="btc-usdt")
   client.start()
   try:
     while True:
